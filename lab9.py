47fcf3fea11a81fde9d6f7d6b8216a1bec2d14b9
Zhe Dong <dongzhe181@outlook.com>
2026-03-09 15:53:46 -0700

    lab9

---
+++ lab9.py
@@ -0,0 +1,23 @@
+
+
+# Challenge 2
+
+def get_next(number):
+    total_sum = 0
+    while number > 0:
+        number, digit = divmod(number, 10)
+        total_sum += digit ** 2
+    return total_sum
+
+def isHappy(n):
+    visited = set()
+
+    while n != 1 and n not in visited:
+        visited.add(n)
+        n = get_next(n)
+
+    return n == 1
+
+
+if __name__ == "__main__":
+    print(isHappy(19))
